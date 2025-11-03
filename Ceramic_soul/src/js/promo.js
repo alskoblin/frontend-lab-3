22bc4c0616598a9091ff693c2be6603848525879
Aleksandr Skoblin <aleksandr.skoblin05@gmail.com>
2025-11-03 19:13:04 +0300

    [lab-3]: fixes

---
+++ Ceramic_soul/src/js/promo.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const vase = document.querySelector('.promo_image_vase');
+    console.log(vase);
+    vase.classList.add('active');
+});
--- Ceramic_soul/src/js/script.js
+++ Ceramic_soul/src/js/script.js
@@ -3,3 +3,4 @@ import './apiback';
 import './slider';
 import './burger';
 import './validation';
+import './promo'
